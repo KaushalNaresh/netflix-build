7007c7a1165bc149931c0c3e9ef73fb862f06c74
KaushalNaresh <kaushalnaresh2689@gmail.com>
2023-01-03 01:54:34 -0800

    front-end-completed-deployed-on:https://netflix-clone-991b6.web.app

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,29 @@ import React from "react";
 import './HomeScreen.css';
 import Nav from "./Nav";
 import Banner from "./Banner";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen(){
     return (
         <div className="homeScreen">
             <Nav />
+
             <Banner />
+
+            <Row 
+                title='NETFLIX ORIGINALS'
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
+            <Row title='Top Rated' fetchUrl={requests.fetchtopRated}
+            />
+            <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
+            <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies}/>
+            <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title='Romance Movies' fetchUrl={requests.fetchRomancemovies}/>
+            <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
         </div>
     )
 }
---
+++ src/Requests.js
@@ -0,0 +1,15 @@
+// typically we would store in {process.env.API_KEY}
+const API_KEY = "82f229329e34cbd69fdd1c301ace2abf";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchtopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomancemovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
